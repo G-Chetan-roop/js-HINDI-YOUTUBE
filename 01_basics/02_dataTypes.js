dee137947a82fea556291db6efc297fd778b56ee
G-Chetan-roop <140826963+G-Chetan-roop@users.noreply.github.com>
2024-03-03 04:29:50 +0000

    disscussed about datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict";//treat all the js code as newer version
+
+//alert( 3 + 3) we are using node js not browser
+
+
+let name = "hitesh"
+let age = 20
+let isLoggedIn = false
+let state 
+// number >=2 
+// null is a datatype which is standalone value meaning empty
+// undefined 
+// symbol unique
+
+// objects 
+
+console.log(typeof "hitesh");
+console.log(typeof null);
+console.log(typeof undefined)
