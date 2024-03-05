8550105c74496264ca75dabb7dc71d89904cf706
G-Chetan-roop <140826963+G-Chetan-roop@users.noreply.github.com>
2024-03-05 04:21:15 +0000

    string methods

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,41 @@
+// //  const name = "Chetan"
+// //  const repoCount = 50
+
+// //  //console.log(name +repoCount + "Value");
+
+// //  console.log(`Hello my name is ${name} and my repCount is ${repoCount}`);
+
+//  const gameName = new String("CHETAN-R")
+
+// //  console.log(gameName[0]);
+// //  console.log(gameName.__proto__);
+
+
+// //  console.log(gameName.length);
+// //  console.log(gameName.toUpperCase())
+//  console.log(gameName.charAt(2))
+//  console.log(gameName.indexOf('C'))
+
+// const newString = gameName.substring(0,4)//cant give - ve values
+// console.log(newString)
+
+// const anotherString = gameName.slice(0,4)//can give -ve values
+// console.log(anotherString)
+
+// const newStringOne = "     Chetan     "
+// console.log(newString)
+// console.log(newStringOne.trim());
+
+// const url = "https://chetan.com/chetan%20chetan"
+
+// console.log(url.replace('%20','-'))
+
+
+// console.log(url.includes('chetan'))
+
+// const s= new String("chetan-roop-svnv")
+// console.log(s.split('-'));
+
+const s = new String("HEllo my name is Chetan")
+console.log(`The index valur of H is ${s.at('0')}`)
+console.log(s.charCodeAt(100))
