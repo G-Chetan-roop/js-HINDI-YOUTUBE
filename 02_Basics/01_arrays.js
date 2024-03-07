4cd80a86298cbe2facf9fce7390f1bc16c6e5720
G-Chetan-roop <140826963+G-Chetan-roop@users.noreply.github.com>
2024-03-07 04:09:19 +0000

    arrays

---
+++ 02_Basics/01_arrays.js
@@ -0,0 +1,45 @@
+// // array
+
+// const myArray = [0, 1, 2, 3, 4, 5]
+// const myHeroes = ["Ironman", "Ghost Rider", "Krish"]
+
+// const myArr2 = new Array(0, 1, 2, 3, 4)
+
+// console.log(myArray[0]);
+
+// // Array methods
+
+// // myArray.push(6)
+// // myArray.push(7)
+// // myArray.pop()
+
+
+// //myArray.unshift(9)
+// //myArray.shift()
+
+// // console.log(myArray.includes(9));
+// // console.log(myArray.indexOf(3));
+
+// const newArr = myArray.join()//join joins the array and converts to string
+
+// // console.log(myArray)
+// // console.log(newArr)
+
+// // slice and splice
+
+// console.log("A ", myArray);
+// const myn1 = myArray.slice(1,3)
+// console.log(myn1)
+// console.log("B", myn1)
+
+// const myn2 = myArray.splice(1,3)
+// console.log(myn2)
+
+// slice doesnt change the original array but splice does
+const A = new Array(0,1,2,3,4,5)
+console.log(A)
+const B = A.slice(1,3)
+console.log(B)
+const C = A.splice(1,3)
+console.log(C)
+console.log(A)
