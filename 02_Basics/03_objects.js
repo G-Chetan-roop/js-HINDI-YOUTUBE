48e00e1d8bb03ea3207d8524153e696e29b818a6
G-Chetan-roop <140826963+G-Chetan-roop@users.noreply.github.com>
2024-03-13 12:02:24 +0000

    objects

---
+++ 02_Basics/03_objects.js
@@ -0,0 +1,32 @@
+// singleton 
+// Object.create
+// object literals
+
+const mySym = Symbol("key1")
+
+const JsUser = {
+    name: "Chetan",
+    "full name": "chetang",
+    [mySym]: "mykey1",
+    age: 18,
+    location: "Hyderabad",
+    email: "1gcet.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday","Friday"]
+}
+
+// console.log(JsUser.email)
+// console.log(JsUser["email"])
+// console.log(JsUser["full name"])
+// console.log(typeof JsUser[mySym])
+
+// JsUser.email = "chetan@google.com"
+// Object.freeze(JsUser)
+// JsUser.email = "chet@google.com"
+// console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log("Hello js User")
+}
+
+console.log(JsUser.greeting());
